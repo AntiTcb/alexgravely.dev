22286b9bb799ef06b9f511533b7bbba3a64cd2cb
Alex Gravely <tcbskater@hotmail.com>
2022-05-31 19:13:26 -0400

    Add Google Analytics (#1)
    * Init commit
    
    * Add GA
    
    * Remove socialinks
    
    * Remove config.kit.target

---
+++ src/lib/Analytics.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { page } from '$app/stores'
+
+    $: {
+      if (typeof gtag !== 'undefined') {
+        gtag('config', 'G-M1CN4YR1VL', {
+          page_title: document.title,
+          page_path: $page.url.href,
+        })
+      }
+    }
+  </script>
+
+  <svelte:head>
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-M1CN4YR1VL">
+    </script>
+    <script>
+      window.dataLayer = window.dataLayer || []
+
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+
+      gtag('js', new Date())
+      gtag('config', 'G-M1CN4YR1VL')
+    </script>
+  </svelte:head>
