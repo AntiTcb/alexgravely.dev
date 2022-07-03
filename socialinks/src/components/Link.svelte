7737d523d92b7c6fea0c91ec522674f6252c5ad9
Alex Gravely <tcbskater@hotmail.com>
2022-07-02 23:40:43 -0400

    Swap adapter to support Vercel (#2)
    * Init commit
    
    * Add vercel support
    
    * Update pnpm lock
    
    * Fix

---
+++ socialinks/src/components/Link.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Icon from '@iconify/svelte';
+
+    export let site;
+    export let siteName;
+    export let url;
+    export let height = 50;
+    export let width = 50;
+</script>
+
+<article class="rounded-full p-4 bg-gray-100 w-fit">
+    <a href="{url}" target="_blank">
+        <Icon icon="logos:{site}" width="{width}" height="{height}" class="inline" />
+    </a>
+</article>
---
+++ socialinks/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Link from '../components/Link.svelte';
+</script>
+
+<svelte:head>
+    <title>galacticatt.com</title>
+</svelte:head>
+
+<section class="flex flex-col flex-no-wrap grow gap-4 w-1/2 max-w-3xl m-auto justify-center items-center">
+    <Link site="twitch" siteName="Twitch" url="https://twitch.tv/galacticatt" />
+    <Link site="instagram-icon" siteName="Instagram" url="https://www.instagram.com/galacticatt.official/" />
+    <Link site="tiktok-icon" siteName="TikTok" url="https://www.tiktok.com/@galacticatt_" />
+    <!-- <Link site="youtube-icon" siteName="YouTube" url="" /> -->
+    <Link site="discord-icon" siteName="Discord" url="https://discord.gg/5wuAmSeFuR" />
+</section>
